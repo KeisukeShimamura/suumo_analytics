30da5c216d60af1dd3e27e3050bc495fedf373b6
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2022-02-11 22:46:33 +0900

    2ldkのみに修正

--- get_data.py
+++ get_data.py
@@ -4,10 +4,10 @@ import time
 import csv
 import os
 
-url_list = ['https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=010&bs=040&ta=01&sc=01101&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&srch_navi=1']
+url_list = ['https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=010&bs=040&ta=01&sc=01101&cb=0.0&ct=9999999&mb=0&mt=9999999&md=07&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=25&pc=50']
 save_dir = 'data'
 output_path_list = ['center.csv']
-num_pages = 179
+num_pages = 21
 
 os.mkdir(save_dir)
 for (url, output_path) in zip(url_list, output_path_list):
